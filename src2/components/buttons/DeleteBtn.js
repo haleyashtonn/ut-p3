37ed22ff39936f58288136965e2ec6256815899f
Ping <pingis@gmail.com>
2019-01-15 22:31:38 -0600

    removed extra src folders

--- src2/components/buttons/DeleteBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function DeleteBtn(props){
-    return(
-    <button type="submit" className="btn-large waves-effect waves-light" title="Click to sign up.">Delete</button>
-    )
-
-}
-export default DeleteBtn
--- src2/components/buttons/LoginBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function LoginBtn(props){
-    return(
-    <button type="submit" className="btn-large waves-effect waves-light" title="Click to sign up.">Login</button>
-    )
-
-}
-export default LoginBtn
